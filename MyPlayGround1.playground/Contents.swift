6cf4a043b4e4f7237adefd762f4e8d7a27c3079a
administrador <administrador@administradors-iMac.local>
2018-12-10 18:05:32 -0800

    priema clase

---
+++ MyPlayGround1.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+var str = "Hello, playground"
+str="bye, play"
+
+print(str)
+
+let abc="clase ios"
+
+var num=1.3
+
+
+struct Person {
+    let firstName : String
+    let lastName: String
+    func sayHello(){
+        print ("hola ")
+    }
+}
+
+let personaA=Person(firstName: "n1",
+                     lastName: "n2")
+
+
+personaA.sayHello()
+
+
+var score=5
+score=score+5
+print(score)
+
+score+=4
+score-=4
+score*=3
+score/=10
+
+var x=3
+var y=5
+var z=7
+x+y*z
+(x+y)*z
+
+//operadores logicosl
+
+let temp=100
+if(temp==100){
+    print("100")
+}
+
+let ruedas=3
+switch ruedas{
+case 1:
+    print ("")
+case 2:
+    print ("bicicleta")
+default:
+    print("+ ruedas")
+
+}
+
+let distancia=80
+
+
+
+
+
+
+
+
+
+
+
